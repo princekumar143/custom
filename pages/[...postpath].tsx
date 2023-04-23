0b77f6c553314bb677dd77086084d34bf2eb397f
princekumar143 <73171027+princekumar143@users.noreply.github.com>
2023-04-23 15:37:49 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -6,12 +6,14 @@ import { GraphQLClient, gql } from 'graphql-request';
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const endpoint = process.env.GRAPHQL_ENDPOINT as string;
   const graphQLClient = new GraphQLClient(endpoint);
+  const referringURL = ctx.req.headers?.referer || null;
   const pathArr = ctx.query.postpath as Array<string>;
   const path = pathArr.join('/');
+  console.log(path);
   const fbclid = ctx.query.fbclid;
 
   // redirect if facebook is the referer or request contains fbclid
-  if (ctx.req.headers?.referer?.includes('facebook.com') || fbclid) {
+  if (referringURL?.includes('facebook.com') || fbclid) {
     return {
       redirect: {
         permanent: false,
@@ -24,11 +26,17 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     {
       post(id: "/${path}/", idType: URI) {
         id
+        dateGmt
+        modifiedGmt
         content
+        author {
+          node {
+            name
+          }
+        }
         featuredImage {
           node {
             sourceUrl
-            altText
           }
         }
       }
@@ -44,6 +52,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
   return {
     props: {
+      path,
       post: data.post,
       host: ctx.req.headers.host,
     },
@@ -53,24 +62,28 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 interface PostProps {
   post: any;
   host: string;
+  path: string;
 }
 
 const Post: React.FC<PostProps> = (props) => {
-  const { post, host } = props;
+  const { post, host, path } = props;
 
   return (
     <>
       <Head>
         <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-        <meta property="og:image:alt" content={post.featuredImage.node.altText} />
+        <link rel="canonical" href={`https://${host}/${path}`} />
+        <meta property="og:url" content={`https://${host}/${path}`} />
         <meta property="og:type" content="article" />
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content={host.split('.')[0]} />
+        <meta property="article:published_time" content={post.dateGmt} />
+        <meta property="article:modified_time" content={post.modifiedGmt} />
       </Head>
       <div className="post-container">
         <img
           src={post.featuredImage.node.sourceUrl}
-          alt={post.featuredImage.node.altText}
+          alt=""
         />
         <article dangerouslySetInnerHTML={{ __html: post.content }} />
       </div>
